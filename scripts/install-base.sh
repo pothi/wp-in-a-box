1f52602b7209c4d1a8e6cad274efb2375d0f2d3f
Pothi Kalimuthu <pothi@tinywp.com>
2017-02-13 17:00:23 +0530

    Integrate direnv

--- scripts/install-base.sh
+++ scripts/install-base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install pre-requisites
+#--- Install pre-requisites ---#
 echo 'Install prerequisites'
 DEBIAN_FRONTEND=noninteractive apt-get install -y vim \
 	unattended-upgrades apt-listchanges \
@@ -13,13 +13,13 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y vim \
     redis-server \
     direnv duplicity
 
-# setup timezone
+#--- setup timezone ---#
 timedatectl set-timezone UTC
 if [ $? != 0 ]; then
 	echo 'Error setting up timezone'
 fi
 
-# Unattended Upgrades
+#--- Unattended Upgrades ---#
 echo 'APT::Periodic::Update-Package-Lists "1";' > /etc/apt/apt.conf.d/20auto-upgrades
 echo 'APT::Periodic::Unattended-Upgrade "1";' > /etc/apt/apt.conf.d/20auto-upgrades
 
@@ -28,17 +28,20 @@ echo 'APT::Periodic::Unattended-Upgrade "1";' > /etc/apt/apt.conf.d/20auto-upgra
 # if the following doesn't work, comment it and then uncomment the above two lines
 sed -i '/\/\/Unattended-Upgrade::Mail\(OnlyOnError\)\?/ s:^//::' /etc/apt/apt.conf.d/50unattended-upgrades
 
-# Common for all users
-echo 'Setting up skel'
-mkdir /etc/skel/.vim
-# touch /etc/skel/.zshrc
-# if ! grep '# Custom Code - PK' /etc/skel/.zshrc ; then
-	# echo '# Custom Code - PK' > /etc/skel/.zshrc
-	# echo 'HISTFILE=~/log/zsh_history' >> /etc/skel/.zshrc
-	# echo 'export EDITOR=vim' >> /etc/skel/.zshrc
-	# echo 'export VISUAL=vim' >> /etc/skel/.zshrc
-# fi
+#--- Setup direnv ---#
+if ! grep 'direnv' /root/.bashrc ; then
+    echo 'eval "$(direnv hook bash)"' >> /root/.bashrc
+fi
 
+#--- Common for all users ---#
+echo 'Setting up skel'
+
+touch /etc/skel/.bashrc
+if ! grep 'direnv' /etc/skel/.bashrc ; then
+    echo 'eval "$(direnv hook bash)"' >> /etc/skel/.bashrc
+fi
+
+mkdir /etc/skel/.vim
 touch /etc/skel/.vimrc
 if ! grep '" Custom Code - PK' /etc/skel/.vimrc ; then
 	echo '" Custom Code - PK' > /etc/skel/.vimrc
@@ -50,12 +53,20 @@ cp /etc/skel/.viminfo /root/
 # cp /etc/skel/.zshrc /root/
 cp /etc/skel/.vimrc /root/
 
+# touch /etc/skel/.zshrc
+# if ! grep '# Custom Code - PK' /etc/skel/.zshrc ; then
+	# echo '# Custom Code - PK' > /etc/skel/.zshrc
+	# echo 'HISTFILE=~/log/zsh_history' >> /etc/skel/.zshrc
+	# echo 'export EDITOR=vim' >> /etc/skel/.zshrc
+	# echo 'export VISUAL=vim' >> /etc/skel/.zshrc
+# fi
 
-# Change Shell
+
+### Change Shell
 # echo 'Changing shell for root to ZSH'
 # chsh --shell /usr/bin/zsh
 
-# Setup some helper tools
+#--- Setup some helper tools ---#
 echo 'Downloading ps_mem.py, mysqltuner and tuning-primer, etc'
 
 PSMEMURL=http://www.pixelbeat.org/scripts/ps_mem.py
@@ -70,7 +81,8 @@ PRIMERURL=https://launchpad.net/mysql-tuning-primer/trunk/1.6-r1/+download/tunin
 wget -q -O /root/scripts/tuning-primer.sh $PRIMERURL
 chmod +x /root/scripts/tuning-primer.sh
 
-# Setup wp cli
+
+#--- Setup wp cli ---#
 echo 'Setting up WP CLI'
 if [ ! -a /usr/local/bin/wp ]; then
 	echo 'Setting up WP CLI'
