6dfb1d0f54504a667c0e026ab4edbddbf3e3a47c
Pothi Kalimuthu <pothi@tinywp.com>
2017-02-13 23:59:40 +0530

    Setup SFTP username and password

--- scripts/create-sftp-user.sh
+++ scripts/create-sftp-user.sh
@@ -17,15 +17,13 @@ fi
 
 #--- please do not edit below this file ---#
 
-SSHD_CONF=/etc/ssh/sshd_config
+SSHD_CONFIG='/etc/ssh/sshd_config'
 
-if [ ! -e "/home/${BASE_NAME}/" ]; then
+if [ ! -d "/home/${BASE_NAME}" ]; then
     groupadd --gid=1010 $WP_SFTP_USER &> /dev/null
     useradd --uid=1010 --gid=1010 --shell=/usr/bin/zsh -m --home-dir /home/${BASE_NAME}/ $WP_SFTP_USER &> /dev/null
 
     groupadd ${BASE_NAME} &> /dev/null
-
-    BASE_NAME=${BASE_NAME}
 else
     echo "the default directory /home/${BASE_NAME} already exists!"; exit 1
 fi
@@ -47,27 +45,27 @@ chmod 755 /home/${BASE_NAME}
 # fi
 # latest way of doing things
 # ref: https://knowledgelayer.softlayer.com/learning/how-do-i-permit-specific-users-ssh-access
-groupadd –r sshusers
+# groupadd –r sshusers
 
 # if AllowGroups line doesn't exist, insert it only once!
-if ! grep -i "AllowGroups" ${SSHD_CONFIG} &> /dev/null ; then
-    echo '
-# allow users within the (system) group "sshusers"
-AllowGroups sshusers
-' >> ${SSHD_CONFIG}
-fi
+# if ! grep -i "AllowGroups" ${SSHD_CONFIG} &> /dev/null ; then
+    # echo '
+# # allow users within the (system) group "sshusers"
+# AllowGroups sshusers
+# ' >> ${SSHD_CONFIG}
+# fi
 
 # add new users into the 'sshusers' now
-usermod -a -G sshusers ${WP_SFTP_USER}
+# usermod -a -G sshusers ${WP_SFTP_USER}
 
 # if the text 'match group ${BASE_NAME}' isn't found, then
 # insert it only once
-if ! grep "Match group ${BASE_NAME}" ${SSHD_CONFIG} &> /dev/null ; then
+if ! grep "Match group ${BASE_NAME}" "${SSHD_CONFIG}" &> /dev/null ; then
     # remove the existing subsystem
     sed -i 's/^Subsystem/### &/' ${SSHD_CONFIG}
 
     # add new subsystem
-echo '
+echo "
 # setup internal SFTP
 Subsystem sftp internal-sftp
     Match group ${BASE_NAME}
@@ -75,20 +73,23 @@ Subsystem sftp internal-sftp
     X11Forwarding no
     AllowTcpForwarding no
     ForceCommand internal-sftp
-' >> ${SSHD_CONFIG}
+" >> ${SSHD_CONFIG}
 
 fi # /Match group ${BASE_NAME}
 
-echo 'Testing the modified SSH config'
-/usr/sbin/sshd –t &> /dev/null
-if [ "$?" != 0 ]; then
-    echo 'Something is messed up in the SSH config file'
-    echo 'Please re-run after fixing errors'
-    echo "See the logfile ${LOG_FILE} for details of the error"
-    echo 'Exiting pre-maturely'
-    exit 1
-else
-    echo 'Cool. Things seem fine. Restarting SSH Daemon...'
+# echo 'Testing the modified SSH config'
+# the following didn't work
+# sshd –t
+# /usr/sbin/sshd -t
+# if [ "$?" != 0 ]; then
+    # echo 'Something is messed up in the SSH config file'
+    # echo 'Please re-run after fixing errors'
+    # echo "See the logfile ${LOG_FILE} for details of the error"
+    # echo 'Exiting pre-maturely'
+    # exit 1
+# else
+    # echo 'Cool. Things seem fine.'
+    echo 'Restarting SSH Daemon...'
     systemctl restart sshd &> /dev/null
     if [ "$?" != 0 ]; then
         echo 'Something went wrong while creating SFTP user! See below...'; echo; echo;
@@ -98,9 +99,12 @@ else
         echo 'WARNING: Try to create another SSH connection from another terminal, just incase...!'
         echo 'Do NOT ignore this warning'
     fi
-fi
+# fi
 
 WP_SFTP_PASS=$(pwgen -s 18 1)
+
+echo "$WP_SFTP_USER:$WP_SFTP_PASS" | chpasswd
+
 echo; echo "SFTP username is $WP_SFTP_USER"; echo;
 echo; echo "SFTP password is $WP_SFTP_PASS"; echo;
 echo 'Please make a note of these somewhere safe'
