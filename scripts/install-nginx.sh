464fc17b198f66b44c51aa7637c3a3314f9629c4
Pothi Kalimuthu <pothi@tinywp.com>
2017-09-07 11:48:02 +0530

    Dynaimc calculation of worker_processes - fixes #18

--- scripts/install-nginx.sh
+++ scripts/install-nginx.sh
@@ -26,6 +26,8 @@ if [ ! -d "$LT_DIRECTORY" ]; then
 	cp -a /etc $LT_DIRECTORY
 fi
 
+sed -i 's/worker_processes.*/worker_processes auto;/g' /etc/nginx/nginx.conf
+
 if [ -d /root/git/wordpress-nginx ] ; then
     cd /root/git/wordpress-nginx && git pull origin master && cd - &> /dev/null
 else
