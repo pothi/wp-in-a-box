620c80b8ddb06bf0c356e6d8d2874cda70f68f27
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2017-11-04 12:27:16 +0530

    Fix crontab creation for pma

--- scripts/phpmyadmin-installation.sh
+++ scripts/phpmyadmin-installation.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-PMA_USER=pma
+export PMA_USER=pma
+export PMA_HOME=/var/www/pma
 
-mkdir -p /var/www
+mkdir -p /var/www &> /dev/null
 
-useradd --home-dir /var/www/html -m $PMA_USER
-chown ${PMA_USER} /var/www/html
+useradd --home-dir $PMA_HOME -m $PMA_USER
+chown ${PMA_USER} $PMA_HOME
 
 if [ ! -f "/var/www/html/.envrc" ]; then
     dbuser=pma$(pwgen -cns 5 1)
     dbpass=$(pwgen -cnsv 8 1)
-    echo "export pma_db_user=$dbuser" > /var/www/html/.envrc
-    echo "export pma_db_pass=$dbpass" >> /var/www/html/.envrc
-    chmod 600 /var/www/html/.envrc
-    chown $PMA_USER /var/www/html/.envrc
-    source /var/www/html/.envrc
+    echo "export pma_db_user=$dbuser" > ${PMA_HOME}/.envrc
+    echo "export pma_db_pass=$dbpass" >> ${PMA_HOME}/.envrc
+    chmod 600 ${PMA_HOME}/.envrc
+    chown $PMA_USER ${PMA_HOME}/.envrc
+    source ${PMA_HOME}/.envrc
 fi
 
 mysql -e "CREATE DATABASE phpmyadmin"
