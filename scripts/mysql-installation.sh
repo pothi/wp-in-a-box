05611133e9b44c4e56ce273a2c1d78bdb9bef184
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-05-06 21:43:04 +0530

    Create a spare SQL admin user - fixes #17

--- scripts/mysql-installation.sh
+++ scripts/mysql-installation.sh
@@ -12,4 +12,18 @@ systemctl stop mysql
 cp $LOCAL_WPINABOX_REPO/config/mariadb.conf.d/*.cnf /etc/mysql/mariadb.conf.d/
 systemctl start mysql
 
+source /root/.envrc
+
+echo 'Setting up MySQL user...'
+
+if [ "$MYSQL_ADMIN_USER" == "" ]; then
+    # create MYSQL username automatically
+    MYSQL_ADMIN_USER="sqladmin_$(pwgen -A 8 1)"
+    MYSQL_ADMIN_PASS=$(pwgen -cnsv 20 1)
+    echo "export MYSQL_ADMIN_USER=$MYSQL_ADMIN_USER" >> /root/.envrc
+    echo "export MYSQL_ADMIN_PASS=$MYSQL_ADMIN_PASS" >> /root/.envrc
+    mysql -e "CREATE USER ${MYSQL_ADMIN_USER}@localhost IDENTIFIED BY '${MYSQL_ADMIN_PASS}';"
+    mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${MYSQL_ADMIN_USER}@localhost WITH GRANT OPTION"
+fi
+
 echo "Done installing MySQL / MariaDB server!"
