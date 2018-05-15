5ca86cf10f44cedc83dafd2712189843b5837281
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-05-15 19:18:16 +0530

    Fix noise when crontab is empty

--- scripts/base-installation.sh
+++ scripts/base-installation.sh
@@ -44,6 +44,18 @@ optional_packages="apt-file \
     # echo "done."
 # done
 
+if [ ! -s /var/spool/cron/crontabs/root ]; then
+    echo '# ┌───────────── minute (0 - 59)
+# │ ┌───────────── hour (0 - 23)
+# │ │ ┌───────────── day of month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                       7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * *  command to execute' | crontab - &> /dev/null
+fi
+
 #--- setup timezone ---#
 echo -n 'Setting up timezone... '
 ln -fs /usr/share/zoneinfo/UTC /etc/localtime
