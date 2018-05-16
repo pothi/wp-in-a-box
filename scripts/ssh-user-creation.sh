6c32f5da54302e00a26ddfbc2b21941562eebc82
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-05-16 21:30:13 +0530

    Fix password auth for ssh user

--- scripts/ssh-user-creation.sh
+++ scripts/ssh-user-creation.sh
@@ -17,6 +17,22 @@ fi
 
 SSHD_CONFIG='/etc/ssh/sshd_config'
 
+groupadd ssh_users &> /dev/null
+
+echo '
+            Match group ssh_users
+            PasswordAuthentication yes
+' >> $SSHD_CONFIG
+
+echo 'Restarting SSH Daemon...'
+systemctl restart sshd &> /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Something went wrong while creating SFTP user! See below...'; echo; echo;
+    systemctl status sshd
+else
+    echo 'SSH Daemon restarted!'
+fi
+
 if [ ! -d "/home/${SSH_USER}" ]; then
     useradd -m $SSH_USER
 
@@ -26,6 +42,8 @@ if [ ! -d "/home/${SSH_USER}" ]; then
     SSH_PASS=$(pwgen -cns 12 1)
 
     echo "$SSH_USER:$SSH_PASS" | chpasswd
+
+    gpasswd -a $SSH_USER ssh_users
 else
     echo "Note: The default directory /home/${SSH_USER} already exists!"
     echo "Note: The user '${SSH_USER}' already exists"
