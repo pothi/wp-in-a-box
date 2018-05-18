118fbae0493e6a7724d19ca548301f8751426ab9
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-05-18 19:20:02 +0530

    Code clean-up

--- scripts/swap.sh
+++ scripts/swap.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-# variables
+
+###   variables   ###
 swap_file='/swapfile'
+swap_size='1G'
 swap_sysctl_file='/etc/sysctl.d/60-swap-local.conf'
 sleep_time_between_tasks=2
-swap_size='1G'
 
-# only create swap if unavailable
+
+# create swap if unavailable
 is_swap_enabled=$(free | grep -iw swap | awk {'print $2'}) # 0 means no swap
-if [ "$is_swap_enabled" -eq 0 ]; then
+if [ $is_swap_enabled -eq 0 ]; then
     echo 'Creating and setting up Swap...'
     echo '-------------------------------------------------------------------------'
 
-    # check for swap
+    # check for swap file
     if [ ! -f $swap_file ]; then
-        # send the output to /dev/null to reduce the noise
         fallocate -l $swap_size $swap_file &> /dev/null
         if [ $? -ne 0 ]; then
-            echo 'Could not create swap file using fllocate. Exiting!'
+            echo Could not create swap file using fallocate.
         fi
     else
-        echo 'Note: Existing swap file found!'
+        echo Note: Existing swap file found!
     fi
 
     # only root should be able to read it
@@ -28,11 +29,10 @@ if [ "$is_swap_enabled" -eq 0 ]; then
 
     # enable swap upon boot
     fstab_entry="$swap_file none swap sw 0 0"
-    # send the output of grep to /dev/null to reduce the noise
     if ! $(grep -q "^${fstab_entry}$" /etc/fstab &> /dev/null) ; then
-        echo $fstab_entry >> /etc/fstab
+        echo $fstab_entry >> /etc/fstab &> /dev/null
     else
-        echo "Note: /etc/fstab already has an entry for swap!"
+        echo Note: /etc/fstab already has an entry for swap!
     fi
 
     mkswap $swap_file
@@ -42,16 +42,15 @@ if [ "$is_swap_enabled" -eq 0 ]; then
     fi
 
     # enable swap
-    echo "Waiting for swap file to get ready..."
+    echo Waiting for swap file to get ready...
     sleep $sleep_time_between_tasks
     swapon -a
     if [ $? -ne 0 ]; then
-        echo 'Error enabling swap using the command "swapon -a". Exiting!'
-        exit 1
+        echo Error enabling swap using the command "swapon -a". Exiting!
     fi
 
     # display summary of swap (only for logging purpose)
-    swapon -s
+    # swapon -s
 
     # fine-tune swap
     # printf "# Ref: https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04\n\nvm.swappiness=10\nvm.vfs_cache_pressure = 50\n" > $swap_sysctl_file
@@ -65,12 +64,11 @@ if [ "$is_swap_enabled" -eq 0 ]; then
     # alternative way
     # sysctl -p $swap_sysctl_file
     if [ $? != 0 ]; then
-        echo 'Error restarting procps!'
-        exit 1
+        echo Error restarting procps!
     fi
 
-    echo '-------------------------------------------------------------------------'
-    echo 'Done setting up swap!'
+    echo -------------------------------------------------------------------------
+    echo Done setting up swap!
 fi
 
 # TODO: Setup alert if swap is used
