c7cd52666b91bbbd1fa31b3152bacb5fa65b9d33
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2018-06-29 12:42:19 +0530

    Fix resetting password for redis

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
+# what's done here
+# 1. install redis, redis for PHP
+# 2. configure redis and restart it
+# 3. configure php and restart it
+# 4. tweak sysctl for redis
+
 source ~/.envrc
 
 # variables
 redis_maxmemory_policy='allkeys-lru'
 redis_conf_file='/etc/redis/redis.conf'
 redis_sysctl_file='/etc/sysctl.d/60-redis-local.conf'
-redis_pass=$(pwgen -cns 20 1)
-# php_version from php-installation.php
 
+restart_redis='no'
+
+redis_pass=$(grep -w '^requirepass' redis.conf | awk '{print $2}')
+if [ -z "$redis_pass" ]; then
+    redis_pass=$(pwgen -cns 20 1)
+
+    # set password
+    sed -i -e 's/^#\? \?\(requirepass\).*$/\1 '$redis_pass'/' $redis_conf_file
+
+    restart_redis='yes'
+
+fi
+
+# php_version from php-installation.php
 FPM_PHP_INI=/etc/php/${php_version}/fpm/php.ini
 
 echo -n 'Installing redis... '
@@ -35,9 +53,6 @@ sed -i -e 's/^#\? \?\(maxmemory \).*$/\1'$redis_memory'm/' $redis_conf_file
 # change the settings for maxmemory-policy
 sed -i -e 's/^#\? \?\(maxmemory\-policy\).*$/\1 '$redis_maxmemory_policy'/' $redis_conf_file
 
-# set password
-sed -i -e 's/^#\? \?\(requirepass\).*$/\1 '$redis_pass'/' $redis_conf_file
-
 # create / overwrite and append our custom values in it
 printf "vm.overcommit_memory = 1\n" > $redis_sysctl_file &> /dev/null
 printf "net.core.somaxconn = 1024\n" >> $redis_sysctl_file &> /dev/null
@@ -46,14 +61,20 @@ printf "net.core.somaxconn = 1024\n" >> $redis_sysctl_file &> /dev/null
 sysctl -p $redis_sysctl_file
 
 # restart redis
-/bin/systemctl restart redis-server
+if [ "yes" == "$restart_redis" ] ; then
+    /bin/systemctl restart redis-server
+else
+    # due to incomplete $restart_redis validation
+    /bin/systemctl restart redis-server
+fi
 
 echo '... done tweaking redis cache.'
 
 # SESSION Handling
 echo 'Setting up PHP sessions to use redis... '
-sed -i -e '/^session.save_handler/ s/=.*/= redis/' $FPM_PHP_INI
-sed -i -e '/^;session.save_path/ s/.*/session.save_path = "tcp:\/\/127.0.0.1:6379?auth='$redis_pass'"/' $FPM_PHP_INI
+[ ! grep '^session.save_handler = redis$' ] && sed -i -e '/^session.save_handler/ s/=.*/= redis/' $FPM_PHP_INI
+sed -i -e '/^;session.save_path/ s/^;//' $FPM_PHP_INI
+sed -i -e '/^session.save_path/ s/.*/session.save_path = "tcp:\/\/127.0.0.1:6379?auth='$redis_pass'"/' $FPM_PHP_INI
 
 /usr/sbin/php-fpm${php_version} -t &> /dev/null && systemctl restart php${php_version}-fpm &> /dev/null
 if [ "$?" != 0 ]; then
