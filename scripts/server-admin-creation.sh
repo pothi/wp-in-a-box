ffca2847d5e7747176555972e9094e92adb45279
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2019-05-16 11:12:27 +0530

    Copy SSH key from root to server_admin_user

--- scripts/server-admin-creation.sh
+++ scripts/server-admin-creation.sh
@@ -13,24 +13,24 @@ if [ "$system_admin_username" == "" ]; then
     echo "export system_admin_username=$system_admin_username" >> /root/.envrc
 fi
 
-#--- please do not edit below this file ---#
-
 SSHD_CONFIG='/etc/ssh/sshd_config'
 
-groupadd ssh_users &> /dev/null
+if ! grep -qw ssh_users $SSHD_CONFIG ; then
+    groupadd ssh_users &> /dev/null
 
-echo '
-            Match group ssh_users
-            PasswordAuthentication yes
-' >> $SSHD_CONFIG
+    echo '
+                Match group ssh_users
+                PasswordAuthentication yes
+    ' >> $SSHD_CONFIG
 
-echo 'Restarting SSH daemon...'
-systemctl restart sshd &> /dev/null
-if [ $? -ne 0 ]; then
-    echo 'Something went wrong while creating SFTP user! See below...'; echo; echo;
-    systemctl status sshd
-else
-    echo ... SSH Daemon restarted!
+    echo 'Restarting SSH daemon...'
+    systemctl restart sshd &> /dev/null
+    if [ $? -ne 0 ]; then
+        echo 'Something went wrong while creating SSH user! See below...'; echo; echo;
+        systemctl status sshd
+    else
+        echo ... SSH Daemon restarted!
+    fi
 fi
 
 if [ ! -d "/home/${system_admin_username}" ]; then
@@ -48,6 +48,10 @@ if [ ! -d "/home/${system_admin_username}" ]; then
 else
     echo "Note: The default directory /home/${system_admin_username} already exists!"
     echo "Note: The user '${system_admin_username}' already exists"
+fi
+
+if [ ! -f /home/${system_admin_username}/.ssh/authorized_keys ]; then
+    cp /root/.ssh/authorized_keys /home/${system_admin_username}/.ssh/authorized_keys
 fi
 
 echo ...done setting up SSH user!
