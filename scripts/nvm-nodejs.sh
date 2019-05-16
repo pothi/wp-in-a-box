8ec7d29fb6f4875854fdb4cc7e52aa779ef3aef4
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2019-05-16 12:22:11 +0530

    Roll-back automatic installation of node via nvm due to an unknown bug

--- scripts/nvm-nodejs.sh
+++ scripts/nvm-nodejs.sh
@@ -5,10 +5,11 @@
 printf '%-72s' "Installing nodejs/npm..."
 if [ ! -s ~/scripts/nvm-install-script.sh ]; then
     curl -L -o ~/scripts/nvm-install-script.sh https://raw.githubusercontent.com/nvm-sh/nvm/$nvm_version/install.sh
-    bash ~/scripts/nvm-install-script.sh
-    export NVM_DIR="~/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # this loads nvm
-    nvm install --lts
+    # Creates an issue with GIT
+    # bash ~/scripts/nvm-install-script.sh
+    # export NVM_DIR="~/.nvm"
+    # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # this loads nvm
+    # nvm install --lts
 fi
 echo done.
 
