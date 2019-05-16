04d4a9860500fa614caf82306e0f9e6c07d523bc
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2019-05-16 16:13:00 +0530

    Fix path info in apt sources

--- scripts/post-install-stretch.sh
+++ scripts/post-install-stretch.sh
@@ -4,7 +4,7 @@
 
 apt-get install certbot -t stretch-backports -q -y
 if [ "$?" -ne "0" ]; then
-    echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.d/backports.list
+    echo 'deb http://deb.debian.org/debian stretch-backports main' > /etc/apt/sources.list.d/backports.list
     apt-get update -qq
     apt-get install certbot -t stretch-backports -q -y
     if [ "$?" -ne "0" ]; then
