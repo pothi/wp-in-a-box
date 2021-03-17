4e6e0eb2327f94f31c54ff3755d8b7e0311e5f20
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2021-03-17 11:52:37 +0530

    Install AWS cli version #2

--- scripts/base-installation.sh
+++ scripts/base-installation.sh
@@ -6,20 +6,33 @@ function install_awscli {
     #----- install AWS cli -----#
     printf '%-72s' "Installing awscli..."
 
-	# for version #1
-	# TODO: Update this function to install version #2
-	# see - https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
-    # remove the previous version of aws cli
+    # for version #1
+    # TODO: Update this function to install version #2
+    # see - https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
+
+    # remove the version #1 of aws cli, if exists
     [ -d /usr/local/aws ] && sudo rm -rf /usr/local/aws
     [ -f /usr/local/bin/aws ] && sudo rm /usr/local/bin/aws
 
-    # using bundle installer
+    # version #1
+    # curl --silent "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
+    # unzip -qq -d /tmp/ /tmp/awscli-bundle.zip
+    # sudo /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws &> /dev/null
+
+    # for version #2
     # ref: https://docs.aws.amazon.com/cli/latest/userguide/install-bundle.html
-    curl --silent "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
-    unzip -qq -d /tmp/ /tmp/awscli-bundle.zip
-    sudo /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws &> /dev/null
-    rm /tmp/awscli-bundle.zip
-    rm -rf /tmp/awscli-bundle
+    curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+    unzip -qq -d /tmp/ /tmp/awscliv2.zip
+    sudo /tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin &> /dev/null # for installation
+    # sudo /tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin --update &> /dev/null # for updates via cron
+
+    # cleanup
+    rm /tmp/awscliv2.zip
+    rm -rf /tmp/aws
+
+    # version #1
+    # rm /tmp/awscli-bundle.zip
+    # rm -rf /tmp/awscli-bundle
     echo done.
 }
 
