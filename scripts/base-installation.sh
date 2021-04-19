fd0db11d9d7a7d4eb41c05b05b3f34fb31e39d1c
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2021-04-19 08:35:54 +0530

    Separate AWS CLI installation script

--- scripts/base-installation.sh
+++ scripts/base-installation.sh
@@ -9,40 +9,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 # variables
 
-function install_awscli {
-    #----- install AWS cli -----#
-    printf '%-72s' "Installing awscli..."
-
-    # for version #1
-    # TODO: Update this function to install version #2
-    # see - https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
-
-    # remove the version #1 of aws cli, if exists
-    [ -d /usr/local/aws ] && sudo rm -rf /usr/local/aws
-    [ -f /usr/local/bin/aws ] && sudo rm /usr/local/bin/aws
-
-    # version #1
-    # curl --silent "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
-    # unzip -qq -d /tmp/ /tmp/awscli-bundle.zip
-    # sudo /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws &> /dev/null
-
-    # for version #2
-    # ref: https://docs.aws.amazon.com/cli/latest/userguide/install-bundle.html
-    curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-    unzip -qq -d /tmp/ /tmp/awscliv2.zip
-    sudo /tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin &> /dev/null # for installation
-    # sudo /tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin --update &> /dev/null # for updates via cron
-
-    # cleanup
-    rm /tmp/awscliv2.zip
-    rm -rf /tmp/aws
-
-    # version #1
-    # rm /tmp/awscli-bundle.zip
-    # rm -rf /tmp/awscli-bundle
-    echo done.
-}
-
 #--- Install pre-requisites ---#
 # landscape-common update-notifier-common \
 echo Installing prerequisites...
@@ -180,4 +146,3 @@ sed -i -e 's/^#EXTRA_OPTS=""$/EXTRA_OPTS=""/' -e 's/^EXTRA_OPTS=""$/EXTRA_OPTS="
 systemctl restart syslog
 systemctl restart cron
 
-which aws || install_awscli
