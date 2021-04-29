4b1faffead70c7c79d8ac8e0f9e6f37bacffec29
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2021-04-29 14:52:48 +0530

    Fix minor issues

--- scripts/linux-tweaks.sh
+++ scripts/linux-tweaks.sh
@@ -5,6 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 local_wp_in_a_box_repo=/root/git/wp-in-a-box
 source ${HOME}/.envrc
 
+[ ! -d ~/.config ] && mkdir ~/.config
+
 # Common shell related configs for root user
 cp $local_wp_in_a_box_repo/snippets/linux/common-aliases-envvars ~/.config/
 source ~/.config/common-aliases-envvars
