1e20b255a022c42b836823c76ea2b12774def0b5
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2022-01-28 12:06:39 +0530

    Add update function for AWS CLI

--- scripts/awscli-install-update-script.sh
+++ scripts/awscli-install-update-script.sh
@@ -17,10 +17,6 @@ function install_awscli {
     # TODO: Update this function to install version #2
     # see - https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 
-    # remove the version #1 of aws cli, if exists
-    [ -d /usr/local/aws ] && sudo rm -rf /usr/local/aws
-    [ -f /usr/local/bin/aws ] && sudo rm /usr/local/bin/aws
-
     # version #1
     # curl --silent "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "/tmp/awscli-bundle.zip"
     # unzip -qq -d /tmp/ /tmp/awscli-bundle.zip
@@ -43,4 +39,26 @@ function install_awscli {
     echo done.
 }
 
+function update_awscli {
+    #----- install AWS cli -----#
+    printf '%-72s' "Updating awscli..."
+
+    # remove the version #1 of aws cli, if exists
+    [ -d /usr/local/aws ] && sudo rm -rf /usr/local/aws
+    [ -f /usr/local/bin/aws ] && sudo rm /usr/local/bin/aws
+
+    # for version #2
+    # ref: https://docs.aws.amazon.com/cli/latest/userguide/install-bundle.html
+    curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+    unzip -qq -d /tmp/ /tmp/awscliv2.zip
+    sudo /tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin --update &> /dev/null
+
+    # cleanup
+    rm /tmp/awscliv2.zip
+    rm -rf /tmp/aws
+
+    echo done.
+}
+
+which aws && update_awscli
 which aws || install_awscli
