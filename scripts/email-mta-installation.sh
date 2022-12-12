8b84438c61bc62d5f2f9a7c387a19ff9c4efc126
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2022-12-12 16:02:43 +0530

    Install pflogsumm by default

--- scripts/email-mta-installation.sh
+++ scripts/email-mta-installation.sh
@@ -29,7 +29,7 @@ echo 'Installing / setting up MTA...'
 
 # dependencies
 # https://serverfault.com/a/325975/102173
-apt-get install -qq $mta libsasl2-modules mailutils &> /dev/null
+apt-get install -qq $mta libsasl2-modules mailutils pflogsumm &> /dev/null
 
 # setup mta to use only ipv4 to send emails
 #- why:
