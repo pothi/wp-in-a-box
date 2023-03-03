99e47225c2139e4ab0350c50b3615dc43956703d
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-03-03 12:49:02 +0530

    Simply logics, add ps_mem.py script to root; merge upstream changes

--- scripts/bootstrap-root.sh
+++ scripts/bootstrap-root.sh
@@ -1,34 +1,30 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Download common-aliases-envvars and insert into ~/.bashrc
 # Configure root-specific ~/.bashrc changes such as terminal color (red for root).
 # Configure crontab, such as certbot renewal.
 # Configure vim
 
-[ ! -d ~/.config ] && mkdir ~/.config
+[ -d ~/.config ] || mkdir ~/.config
 
-[ ! -s ~/.config/common-aliases-envvars ] && wget -q -O ~/.config/common-aliases-envvars https://raw.githubusercontent.com/pothi/snippets/main/linux/common-aliases-envvars
+[ -s ~/.config/common-aliases-envvars ] || wget -q -O ~/.config/common-aliases-envvars https://raw.githubusercontent.com/pothi/snippets/main/linux/common-aliases-envvars
 . ~/.config/common-aliases-envvars
 
 if ! grep -qw common-aliases.envvars ~/.bashrc ; then
-    printf "[[ -f ~/.config/common-aliases-envvars ]] && source ~/.config/common-aliases-envvars\n" >> ~/.bashrc
-fi
-
-if ! grep -qF custom-aliases-envvars-custom ~/.bashrc ; then
-    echo "[[ -f ~/.config/custom-aliases-envvars-custom ]] && . ~/.config/custom-aliases-envvars-custom" >> ~/.bashrc
+    echo -e "\n[ -f ~/.config/common-aliases-envvars ] && source ~/.config/common-aliases-envvars\n" >> ~/.bashrc
 fi
 
 # Load ~/.envrc file if exists
 if ! grep -qF envrc ~/.bashrc ; then
-    echo "[ -f ~/.envrc ] && . ~/.envrc" >> ~/.bashrc
+    echo -e "\n[ -f ~/.envrc ] && . ~/.envrc\n" >> ~/.bashrc
 fi
 
 ###------------------------------ setup color for root terminal ------------------------------###
 rootbashrc=/root/.bashrc
 entry='#red_color for root'
 if [ -f $rootbashrc ]; then
-    if ! $(grep -q "^${entry}$" "$rootbashrc") ; then
-        printf "\n${entry}\n" >> $rootbashrc
+    if ! grep -q "^${entry}$" "$rootbashrc" ; then
+        echo -e "\n${entry}\n" >> $rootbashrc
         echo 'PS1="\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;31m\]\u\[\033[01;33m\]@\[\033[01;36m\]\h \[\033[01;33m\]\w \[\033[01;35m\]\$ \[\033[00m\]"' >> $rootbashrc
         printf '\n' >> $rootbashrc
         . $rootbashrc
@@ -36,10 +32,22 @@ if [ -f $rootbashrc ]; then
 fi # test if file exists
 
 ###------------------------------ VIM Tweaks ------------------------------###
-#TODO
-[ ! -d ~/.vim ] && mkdir ~/.vim
-[ ! -d ~/git/snippets ] && {
+[ -d ~/.vim ] || mkdir ~/.vim
+[ -d ~/git/snippets ] || {
     git clone -q --depth 1 https://github.com/pothi/snippets ~/git/snippets
     cp -a ~/git/snippets/vim/* ~/.vim/
 }
 
+[ -d ~/.local/bin ] || mkdir -p ~/.local/bin
+ps_mem_file=~/.local/bin/ps_mem.py
+if [ ! -f "$ps_mem_file" ]; then
+    printf '%-72s' "Downloading ps_mem.py script..."
+    script_url=http://www.pixelbeat.org/scripts/ps_mem.py
+    wget -q -O ~/.local/bin/ps_mem.py $script_url
+    # check_result $? 'ps_mem.py: error downloading the script.'
+    chmod +x "$ps_mem_file"
+    echo done.
+fi
+
+# TODO: Download tuning-primer, mysqlturner scripts
+# have a cron to periodically run the above scripts with logs saved every day.
