0659e09f5f3de161ae758e71c47eeec9f8e1cc56
Pothi Kalimuthu <1254302+pothi@users.noreply.github.com>
2023-07-10 08:56:11 +0530

    Improve compatibility for .envrc file

--- bootstrap-debian-bullseye.sh
+++ bootstrap-debian-bullseye.sh
@@ -3,7 +3,10 @@
 # programming env: these switches turn some bugs into errors
 # set -o errexit -o pipefail -o noclobber -o nounset
 
-# Version: 2.3
+# Version: 3.0
+
+# this is the PHP version that comes by default with the current Ubuntu LTS
+php_ver=7.4
 
 # to be run as root, probably as a user-script just after a server is installed
 # https://stackoverflow.com/a/52586842/1004587
@@ -13,9 +16,10 @@ is_user_root () { [ "${EUID:-$(id -u)}" -eq 0 ]; }
 
 export PATH=~/bin:~/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin
 export DEBIAN_FRONTEND=noninteractive
+
 echo "Script started on (date & time): $(date +%c)"
 
-# Defining return code check function
+# Function to exit with an error message
 check_result() {
     if [ $? -ne 0 ]; then
         echo; echo "Error: $1"; echo
@@ -23,20 +27,48 @@ check_result() {
     fi
 }
 
-[ ! -f "$HOME/.envrc" ] && touch ~/.envrc
-. ~/.envrc
+# function to configure timezone to UTC
+set_utc_timezone() {
+    if [ "$(date +\%Z)" != "UTC" ] ; then
+        [ ! -f /usr/sbin/tzconfig ] && apt-get -qq install tzdata > /dev/null
+        printf '%-72s' "Setting up timezone..."
+        ln -fs /usr/share/zoneinfo/UTC /etc/localtime
+        dpkg-reconfigure -f noninteractive tzdata
+        # timedatectl set-timezone UTC
+        check_result $? 'Error setting up timezone.'
 
-git_usermail=${EMAIL:-root@localhost}
-git_username=${NAME:-root}
+        # Recommended to restart cron after every change in timezone
+        systemctl restart cron
+        check_result $? 'Error restarting cron daemon after changing timezone.'
+        echo done.
+    fi
+}
+
+# if ~/.envrc doesn't exist, create it
+if [ ! -f "$HOME/.envrc" ]; then
+    touch ~/.envrc
+    chmod 600 ~/.envrc
+# if exists, source it to apply the env variables
+else
+    . ~/.envrc
+fi
+
+#--- apt tweaks ---#
 
 # Ref: https://wiki.debian.org/Multiarch/HOWTO
 # https://askubuntu.com/a/1336013/65814
 [ ! $(dpkg --get-selections | grep -q i386) ] && dpkg --remove-architecture i386 2>/dev/null
 
 # Fix apt ipv4/6 issue
+[ ! -f /etc/apt/apt.conf.d/1000-force-ipv4-transport ] && \
 echo 'Acquire::ForceIPv4 "true";' > /etc/apt/apt.conf.d/1000-force-ipv4-transport
 
+# Fix a warning related to dialog
+# run `debconf-show debconf` to see the current /default selections.
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 # the following runs when apt cache is older than 6 hours
+# Taken from Ansible - https://askubuntu.com/a/1362550/65814
 APT_UPDATE_SUCCESS_STAMP_PATH=/var/lib/apt/periodic/update-success-stamp
 APT_LISTS_PATH=/var/lib/apt/lists
 if [ -f "$APT_UPDATE_SUCCESS_STAMP_PATH" ]; then
@@ -64,7 +96,11 @@ if [ "$lang" != "en_US.UTF-8" ]; then
     source /etc/default/locale
 fi
 
-echo -------------------------- Prerequisites ------------------------------------
+# -------------------------- Prerequisites ------------------------------------
+
+# apt-utils to fix an annoying non-critical bug on minimal images. Ref: https://github.com/tianon/docker-brew-ubuntu-core/issues/59
+apt-get -qq install apt-utils &> /dev/null
+
 required_packages="apt-transport-https \
     curl \
     dnsutils \
@@ -75,7 +111,6 @@ required_packages="apt-transport-https \
     snapd \
     software-properties-common \
     sudo \
-    tzdata \
     unzip \
     wget"
 
@@ -94,23 +129,99 @@ do
 done
 
 #--- setup timezone ---#
-current_time_zone=$(date +\%Z)
-if [ "$current_time_zone" != "UTC" ] ; then
-    printf '%-72s' "Setting up timezone..."
-    ln -fs /usr/share/zoneinfo/UTC /etc/localtime
-    dpkg-reconfigure -f noninteractive tzdata
-    # timedatectl set-timezone UTC
-    check_result $? 'Error setting up timezone.'
-    systemctl restart cron
-    check_result $? 'Error restarting cron daemon.'
-    echo done.
+set_utc_timezone
+
+# Create a WordPress user with /home/web as $HOME
+wp_user=${WP_USERNAME:-""}
+if [ "$wp_user" == "" ]; then
+printf '%-72s' "Creating a WP User..."
+    wp_user="wp_$(openssl rand -base64 32 | tr -d /=+ | cut -c -10)"
+    echo "export WP_USERNAME=$wp_user" >> /root/.envrc
+echo done.
 fi
+
+# home_basename=$(echo $wp_user | awk -F _ '{print $1}')
+# [ -z $home_basename ] && home_basename=web
+home_basename=web
+
+useradd --shell=/bin/bash -m --home-dir /home/${home_basename} $wp_user
+chmod 755 /home/$home_basename
+
+groupadd ${home_basename}
+gpasswd -a $wp_user ${home_basename} > /dev/null
+
+# Create password for WP User
+wp_pass=${WP_PASSWORD:-""}
+if [ "$wp_pass" == "" ]; then
+printf '%-72s' "Creating password for WP user..."
+    wp_pass=$(openssl rand -base64 32 | tr -d /=+ | cut -c -20)
+    echo "export WP_PASSWORD=$wp_pass" >> /root/.envrc
+echo done.
+fi
+
+echo "$wp_user:$wp_pass" | chpasswd
+
+# provide sudo access without passwd to WP User
+if [ ! -f /etc/sudoers.d/$wp_user ]; then
+printf '%-72s' "Providing sudo privilege for WP user..."
+    echo "${wp_user} ALL=(ALL) NOPASSWD:ALL"> /etc/sudoers.d/$wp_user
+    chmod 400 /etc/sudoers.d/$wp_user
+echo done.
+fi
+
+# Enable password authentication for WP User
+cd /etc/ssh/sshd_config.d
+if [ ! -f enable-passwd-auth.conf ]; then
+printf '%-72s' "Enabling Password Authentication for WP user..."
+    echo "PasswordAuthentication yes" > enable-passwd-auth.conf
+    /usr/sbin/sshd -t && systemctl restart sshd
+    check_result $? 'Error restarting SSH daemon while enabling passwd auth.'
+echo done.
+fi
+cd - 1> /dev/null
 
 echo ---------------------------------- LEMP -------------------------------------
 
-php_ver=7.4
+# ------------------------------- MySQL ---------------------------------------
+# MySQL is required by PHP. So, install it before PHP
+
+package=default-mysql-server
+if dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -q "ok installed"
+then
+    # echo "'$package' is already installed."
+    :
+else
+    printf '%-72s' "Installing '${package}' ..."
+    apt-get -qq install $package > /dev/null
+    check_result "Error: couldn't install $package."
+    echo done.
+fi
+
+# Create a MySQL admin user
+sql_user=${MYSQL_ADMIN_USER:-""}
+if [ "$sql_user" == "" ]; then
+printf '%-72s' "Creating a MySQL Admin User..."
+    # create MYSQL username automatically
+    # unique username / password generator: https://unix.stackexchange.com/q/230673/20241
+    sql_user="mysql_$(openssl rand -base64 32 | tr -d /=+ | cut -c -10)"
+    echo "export MYSQL_ADMIN_USER=$sql_user" >> /root/.envrc
+echo done.
+fi
+
+sql_pass=${MYSQL_ADMIN_PASS:-""}
+if [ "$sql_pass" == "" ]; then
+    sql_pass=$(openssl rand -base64 32 | tr -d /=+ | cut -c -20)
+    echo "export MYSQL_ADMIN_PASS=$sql_pass" >> /root/.envrc
+fi
+
+mysql -e "CREATE USER IF NOT EXISTS ${sql_user} IDENTIFIED BY '${sql_pass}';"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${sql_user} WITH GRANT OPTION"
+
+echo -------------------------------- PHP ----------------------------------------
+
+# PHP is required by Nginx to configure the defaults. So, install it along with Nginx
+
 lemp_packages="nginx-extras \
-    default-mysql-server \
     php${php_ver}-fpm \
     php${php_ver}-mysql \
     php${php_ver}-gd \
@@ -128,7 +239,7 @@ for package in $lemp_packages
 do
     if dpkg-query -W -f='${Status}' $package 2>/dev/null | grep -q "ok installed"
     then
-        echo "'$package' is already installed"
+        # echo "'$package' is already installed"
         :
     else
         # Remove ${php_ver} from package name to find if php-package is installed.
@@ -146,12 +257,24 @@ do
     fi
 done
 
-[ ! -d ~/git/wordpress-nginx ] && {
-    git clone -q https://github.com/pothi/wordpress-nginx ~/git/wordpress-nginx
-    cp -a ~/git/wordpress-nginx/{conf.d,errors,globals,sites-available} /etc/nginx/
+# Download WordPress Nginx repo
+[ ! -d ~/wp-nginx ] && {
+    mkdir ~/wp-nginx
+    wget -q -O- https://github.com/pothi/wordpress-nginx/tarball/main | tar -xz -C ~/wp-nginx --strip-components=1
+    cp -a ~/wp-nginx/{conf.d,errors,globals,sites-available} /etc/nginx/
     [ ! -d /etc/nginx/sites-enabled ] && mkdir /etc/nginx/sites-enabled
     ln -fs /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/default.conf
 }
+
+# Remove the default conf file supplied by OS
+[ -f /etc/nginx/sites-enabled/default ] && rm /etc/nginx/sites-enabled/default
+
+# Remove the default SSL conf to support latest SSL conf.
+# It should hide two lines starting with ssl_
+# ^ starting with...
+# \s* matches any number of space or tab elements before ssl_
+# when run more than once, it just doesn't do anything as the start of the line is '#' after the first execution.
+sed -i 's/^\s*ssl_/# &/' /etc/nginx/nginx.conf 
 
 # create dhparam
 if [ ! -f /etc/nginx/dhparam.pem ]; then
@@ -163,67 +286,6 @@ echo ---------------------------------------------------------------------------
 echo "Please check ~/.envrc for all the credentials."
 echo -----------------------------------------------------------------------------
 
-if [ "$ADMIN_USER" == "" ]; then
-printf '%-72s' "Creating a MySQL Admin User..."
-    # create MYSQL username automatically
-    ADMIN_USER="admin_$(pwgen -Av 6 1)"
-    ADMIN_PASS=$(pwgen -cnsv 20 1)
-    echo "export ADMIN_USER=$ADMIN_USER" >> /root/.envrc
-    echo "export ADMIN_PASS=$ADMIN_PASS" >> /root/.envrc
-    mysql -e "CREATE USER ${ADMIN_USER} IDENTIFIED BY '${ADMIN_PASS}';"
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO ${ADMIN_USER} WITH GRANT OPTION"
-echo done.
-echo "Please check ~/.envrc for credentials."
-fi
-
-wp_user=${WP_USERNAME:-""}
-if [ "$wp_user" == "" ]; then
-printf '%-72s' "Creating a WP User..."
-    wp_user="web_$(pwgen -Av 8 1)"
-    echo "export WP_USERNAME=$wp_user" >> /root/.envrc
-echo done.
-fi
-
-home_basename=$(echo $wp_user | awk -F _ '{print $1}')
-[ -z $home_basename ] && home_basename=web
-
-if [ ! -d "/home/${home_basename}" ]; then
-    useradd --shell=/bin/bash -m --home-dir /home/${home_basename} $wp_user
-
-    groupadd ${home_basename}
-    gpasswd -a $wp_user ${home_basename} &> /dev/null
-fi
-
-wp_pass=${WP_PASSWORD:-""}
-if [ "$wp_pass" == "" ]; then
-printf '%-72s' "Creating password for WP user..."
-    wp_pass=$(pwgen -cns 12 1)
-    echo "export WP_PASSWORD=$wp_pass" >> /root/.envrc
-
-    echo "$wp_user:$wp_pass" | chpasswd
-echo done.
-fi
-
-# provide sudo access without passwd to WP Dev
-if [ ! -f /etc/sudoers.d/$wp_user ]; then
-printf '%-72s' "Providing sudo privilege for WP user..."
-    echo "${wp_user} ALL=(ALL) NOPASSWD:ALL"> /etc/sudoers.d/$wp_user
-    chmod 400 /etc/sudoers.d/$wp_user
-echo done.
-fi
-
-if [ -d /etc/ssh/sshd_config.d ] ; then
-    cd /etc/ssh/sshd_config.d
-    if [ ! -f enable-passwd-auth.conf ]; then
-    printf '%-72s' "Enabling Password Authentication for WP user..."
-        echo "PasswordAuthentication yes" > enable-passwd-auth.conf
-        /usr/sbin/sshd -t && systemctl restart sshd
-        check_result $? 'Error restarting SSH daemon while enabling passwd auth.'
-    echo done.
-    fi
-    cd - 1> /dev/null
-fi
-
 # . $local_wp_in_a_box_repo/scripts/php-installation.sh
 php_user=$wp_user
 fpm_ini_file=/etc/php/${php_ver}/fpm/php.ini
@@ -231,7 +293,7 @@ pool_file=/etc/php/${php_ver}/fpm/pool.d/${php_user}.conf
 PM_METHOD=ondemand
 
 user_mem_limit=${PHP_MEM_LIMIT:-""}
-[ -z "$user_mem_limit" ] && user_mem_limit=256
+[ -z "$user_mem_limit" ] && user_mem_limit=512
 
 max_children=${PHP_MAX_CHILDREN:-""}
 
@@ -296,12 +358,10 @@ sed -i -e '/^listen.mode = / s/[0-9]\{4\}/0666/' $pool_file
 php_ver_short=$(echo $php_ver | sed 's/\.//')
 socket=/run/php/fpm-${php_ver_short}-${php_user}.sock
 sed -i "/^listen =/ s:=.*:= $socket:" $pool_file
-[ -f /etc/nginx/conf.d/lb.conf ] && sed -i "s:/var/lock/php-fpm.*;:$socket;:" /etc/nginx/conf.d/lb.conf
-if [ ! -f /etc/nginx/conf.d/fpm${php_ver_short}.conf ]; then
-    echo "upstream fpm${php_ver_short} { server unix:$socket; }" > /etc/nginx/conf.d/fpm${php_ver_short}.conf
-    echo "upstream fpm { server unix:$socket; }" > /etc/nginx/conf.d/fpm.conf
-    [ -f /etc/nginx/conf.d/lb.conf ] && rm /etc/nginx/conf.d/lb.conf
-fi
+# [ -f /etc/nginx/conf.d/lb.conf ] && sed -i "s:/var/lock/php-fpm.*;:$socket;:" /etc/nginx/conf.d/lb.conf
+[ -f /etc/nginx/conf.d/lb.conf ] && rm /etc/nginx/conf.d/lb.conf
+[ ! -f /etc/nginx/conf.d/fpm.conf ] && echo "upstream fpm { server unix:$socket; }" > /etc/nginx/conf.d/fpm.conf
+[ ! -f /etc/nginx/conf.d/fpm${php_ver_short}.conf ] && echo "upstream fpm${php_ver_short} { server unix:$socket; }" > /etc/nginx/conf.d/fpm${php_ver_short}.conf
 
 sed -i -e 's/^pm = .*/pm = '$PM_METHOD'/' $pool_file
 sed -i '/^pm.max_children/ s/=.*/= '$max_children'/' $pool_file
@@ -355,6 +415,16 @@ apt-get -qq remove certbot
 snap install --classic certbot
 ln -fs /snap/bin/certbot /usr/bin/certbot
 
+# register certbot account if email is supplied
+if [ $CERTBOT_ADMIN_EMAIL ]; then
+    certbot show_account &> /dev/null
+    if [ "$?" != "0" ]; then
+        certbot -m $CERTBOT_ADMIN_EMAIL --agree-tos --no-eff-email register
+    fi
+fi
+
+# Restart script upon renewal; it can also alert upon success or failure
+# See - https://github.com/pothi/snippets/blob/main/ssl/nginx-restart.sh
 [ ! -d /etc/letsencrypt/renewal-hooks/deploy/ ] && mkdir -p /etc/letsencrypt/renewal-hooks/deploy/
 restart_script=/etc/letsencrypt/renewal-hooks/deploy/nginx-restart.sh
 restart_script_url=https://github.com/pothi/snippets/raw/main/ssl/nginx-restart.sh
